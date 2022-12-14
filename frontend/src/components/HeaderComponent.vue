4301c29ca14ae1686cf4a73a5a143dc4087bd29e
Raissa Cardoso <raissa_cardoso@yahoo.com.br>
2022-12-14 14:13:43 -0300

    feat: update frontend

---
+++ frontend/src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+}
+</script>
+
+<style scoped>
+    header{
+        background-color: var(--color-background-nav);
+        height: 60px;
+        width: 100%;
+    }
+
+</style>
