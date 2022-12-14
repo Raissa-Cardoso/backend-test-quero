f7cd54bbb8919d448bce99a597d4b72471b95470
Raissa Cardoso <raissa_cardoso@yahoo.com.br>
2022-12-14 10:47:53 -0300

    feat: add devise

--- backend/Bilinho/config/environments/development.rb
+++ backend/Bilinho/config/environments/development.rb
@@ -53,6 +53,13 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Letter_opener
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perfom_deliveries = true
+
+  #Devise
+  config.action_mailer.default_url_options = {host: 'localhost', port: 3000}  
+
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
@@ -62,4 +69,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+ 
 end
